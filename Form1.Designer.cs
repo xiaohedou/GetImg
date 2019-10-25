e192aa99a56fc5c77ba5e4123444e7899eeae4f5
wangxiaoke <wangxiaoke68@hotmail.com>
2019-10-25 16:46:03 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace GetImg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.DarkViolet;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button7.ForeColor = System.Drawing.Color.White;
+            this.button7.Location = new System.Drawing.Point(426, 111);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(87, 40);
+            this.button7.TabIndex = 19;
+            this.button7.Text = "查看";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.ForestGreen;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button8.ForeColor = System.Drawing.Color.White;
+            this.button8.Location = new System.Drawing.Point(318, 111);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(87, 40);
+            this.button8.TabIndex = 18;
+            this.button8.Text = "提取图片";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(22, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(133, 14);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "选择图片保存路径：";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox4.Location = new System.Drawing.Point(161, 69);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(274, 23);
+            this.textBox4.TabIndex = 15;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.DodgerBlue;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Font = new System.Drawing.Font("微软雅黑", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button6.ForeColor = System.Drawing.Color.White;
+            this.button6.Location = new System.Drawing.Point(452, 66);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(61, 29);
+            this.button6.TabIndex = 16;
+            this.button6.Text = "选择";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(57, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 14);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "选择PDF路径：";
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Brown;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("微软雅黑", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button5.ForeColor = System.Drawing.Color.White;
+            this.button5.Location = new System.Drawing.Point(452, 18);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(61, 30);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "选择";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox3.Location = new System.Drawing.Point(161, 22);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(274, 23);
+            this.textBox3.TabIndex = 13;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.progressBar1.Location = new System.Drawing.Point(0, 163);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(556, 23);
+            this.progressBar1.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(556, 186);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox3);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "图片提取工具";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Spire.Pdf;
+
+namespace GetImg
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;//为了能够跨线程调用控件
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (listPdfDir .Count >0 && textBox4.Text!="")
+            {
+                foreach (string strPath in listPdfDir)
+                {
+                    Directory.CreateDirectory(textBox4.Text.Trim(new char[] { '\\' }) + "\\" + Path.GetFileNameWithoutExtension(strPath));
+                }
+                progressBar1.Value = 0;//设置进度条初始值为0
+                progressBar1.Maximum = listPdfDir.Count;//设置进度条最大值为要操作文件的个数
+                System.Threading.ThreadPool.QueueUserWorkItem(//使用线程池
+                     (P_temp) =>
+                     {
+                         button8.Enabled = false;
+                         foreach (string strPath in listPdf)
+                         {
+                             PdfDocument doc = new PdfDocument();
+                             doc.LoadFromFile(strPath);
+
+                             #region 使用字典存储所有图片及对应页码
+                             Dictionary<Image, string> images = new Dictionary<Image, string>();
+                             for (int i = 0; i < doc.Pages.Count; i++)
+                             {
+                                 if (doc.Pages[i].ExtractImages() != null)
+                                 {
+                                     foreach (Image image in doc.Pages[i].ExtractImages())
+                                     {
+                                         images.Add(image, (i + 1).ToString("000"));
+                                     }
+                                 }
+                             }
+                             #endregion
+                             doc.Close();
+
+                             int index = 1;//图片编号
+                             string page, tempPage = "1";//当前页码/上一页页码，为了图片重新编号
+                             Image tempImage;//当前图片
+                             string imageFileName, imageID;//要保存的图片文件名/图片编号
+                             #region 遍历字典中存储的所有图片及对应页码，并保存为图片
+                             foreach (var image in images)
+                             {
+                                 page = image.Value;
+                                 tempImage = image.Key;
+                                 if (page != tempPage)
+                                     index = 1;
+                                 imageID = index++.ToString("000");
+                                 imageFileName = String.Format(textBox4.Text.Trim(new char[] { '\\' }) + "\\" + Path.GetFileNameWithoutExtension(strPath) + "\\" + "第" + page + "页-{0}.png", imageID);
+                                 tempImage.Save(imageFileName, ImageFormat.Png);
+                                 tempImage.Dispose();
+                                 tempPage = page;
+                             }
+                             #endregion
+                             progressBar1.Value += 1;//设置进度条的值加1
+                         }
+                         //成功提示
+                         MessageBox.Show("提取完成！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                         button8.Enabled = true;
+                     });
+            }
+            else
+            {
+                MessageBox.Show("请选择PDF文档路径！", "温馨提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                button5.Focus();
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(textBox4.Text);//打开重命名后的路径进行查看
+        }
+
+        ArrayList listPdfDir = new ArrayList(),listPdf=new ArrayList();
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dir = new FolderBrowserDialog();//创建浏览文件对话框
+            if (dir.ShowDialog() == DialogResult.OK)//判断是否选择了路径
+            {
+                textBox4.Text = dir.SelectedPath;//显示选择的路径
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            listPdfDir.Clear();
+            FolderBrowserDialog dir = new FolderBrowserDialog();//创建浏览文件对话框
+            if (dir.ShowDialog() == DialogResult.OK)//判断是否选择了路径
+            {
+                textBox3.Text = dir.SelectedPath;//显示选择的路径
+                //存储所有文件夹路径
+                foreach (string str in Directory.GetFiles(textBox3.Text, "*.pdf"))
+                {
+                    listPdfDir.Add(Path.GetFileNameWithoutExtension(str));
+                    listPdf.Add(str);
+                }
+            }
+        }
+    }
+}
